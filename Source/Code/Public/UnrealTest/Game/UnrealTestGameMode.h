c84e897ba4b60a7b7d172662e15cfd12f44a0151
Javier Rolff <javier.rolff@thebreachstudios.com>
2022-05-11 16:03:50 +0200

    Template Third person view game

---
+++ Source/Code/Public/UnrealTest/Game/UnrealTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealTestGameMode();
+};
+
+
+
---
+++ Source/Code/UnrealTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealTest : ModuleRules
+{
+	public UnrealTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
