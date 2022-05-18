419c0b12f08b536faaf7a6918d238ce828c1600d
hobbitct <hobbitct23@outlook.com>
2022-05-18 15:17:54 +0200

    Module dependences for UMG
    UMG module dependences for possible future uses.

--- Source/Code/Private/UnrealTest/Game/UnrealTestGameMode.cpp
+++ Source/Code/Private/UnrealTest/Game/UnrealTestGameMode.cpp
@@ -13,3 +13,26 @@ AUnrealTestGameMode::AUnrealTestGameMode()
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+
+void AUnrealTestGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void AUnrealTestGameMode::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+    if (CurrentWidget != nullptr)
+    {
+        CurrentWidget->RemoveFromViewport();
+        CurrentWidget = nullptr;
+    }
+    if (NewWidgetClass != nullptr)
+    {
+        CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+        if (CurrentWidget != nullptr)
+        {
+            CurrentWidget->AddToViewport();
+        }
+    }
+}
