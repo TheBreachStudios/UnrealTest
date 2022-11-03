19fcda20c85a70bab55ec609cf957b8b0ed042d1
Bokuda <toni.vaquer@gmail.com>
2022-11-03 22:04:15 +0100

    Basic features cleared
    [+] Be able to host a match and have another client that connects.
    [+] Have at least 1 champion available. (All clients can use the same)
    [+] Be able to fight against each other.
    [+] Consider network latency issues when implementing shooting.

--- Source/Code/Public/UnrealTest/Character/UnrealTestCharacter.h
+++ Source/Code/Public/UnrealTest/Character/UnrealTestCharacter.h
@@ -21,6 +21,9 @@ class AUnrealTestCharacter : public ACharacter
 public:
 	AUnrealTestCharacter();
 
+	/** Property replication */
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Input)
 	float TurnRateGamepad;
@@ -72,6 +75,7 @@ public:
 	void TurnBinding(class UInputComponent* PlayerInputComponent);
 	void LookUpBinding(class UInputComponent* PlayerInputComponent);
 	void TouchBinding(class UInputComponent* PlayerInputComponent);
+	void ActionBinding(class UInputComponent* PlayerInputComponent);
 
 	const float TURN_RATE_GAMEPAD = 50.f;
 	const float JUMP_Z_VELOCITY= 700.f;
@@ -79,5 +83,67 @@ public:
 	const float MAX_WALK_SPEED = 500.f;
 	const float MIN_ANALOG_WALK_SPEED = 20.f;
 	const float BRAKING_DECELERATION_WALKING = 2000.f;
+
+protected:
+
+	/** The player's maximum health. This is the highest value of their health can be. This value is a value of the player's health, which starts at when spawned.*/
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+		float MaxHealth;
+
+	/** The player's current health. When reduced to 0, they are considered dead.*/
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentHealth)
+		float CurrentHealth;
+
+	/** RepNotify for changes made to current health.*/
+	UFUNCTION()
+		void OnRep_CurrentHealth();
+
+	/** Response to health being updated. Called on the server immediately after modification, and on clients in response to a RepNotify*/
+	void OnHealthUpdate();
+
+public:
+
+	/** Getter for Max Health.*/
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetMaxHealth() const { return MaxHealth; }
+
+	/** Getter for Current Health.*/
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetCurrentHealth() const { return CurrentHealth; }
+
+	/** Setter for Current Health. Clamps the value between 0 and MaxHealth and calls OnHealthUpdate. Should only be called on the server.*/
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void SetCurrentHealth(float healthValue);
+
+	/** Event for taking damage. Overridden from APawn.*/
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		float TakeDamage(float DamageTaken, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay|Projectile")
+		TSubclassOf<class AUnrealTestProjectile> ProjectileClass;
+
+	/** Delay between shots in seconds. Used to control fire rate for your test projectile, but also to prevent an overflow of server functions from binding SpawnProjectile directly to input.*/
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+		float FireRate;
+
+	/** If true, you are in the process of firing projectiles. */
+	bool bIsFiringWeapon;
+
+	/** Function for beginning weapon fire.*/
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+		void StartFire();
+
+	/** Function for ending weapon fire. Once this is called, the player can use StartFire again.*/
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+		void StopFire();
+
+	/** Server function for spawning projectiles.*/
+	UFUNCTION(Server, Reliable)
+		void HandleFire();
+
+	/** A timer handle used for providing the fire rate delay in-between spawns.*/
+	FTimerHandle FiringTimer;
 };
 
